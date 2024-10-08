144f54b8ebe84d9ebd0341219bd3cbaac83e27ec
rain293 <twilike9x@gmail.com>
2024-10-08 11:09:16 +0900

    a

--- hello.py
+++ hello.py
@@ -1,23 +1,7 @@
-# Ví dụ chuỗi
-paragraph	= """This-is	line	1
-This	is-	line	2-
-This	-is	line	3"""
+numbers = [5, 2, 3, 99, 5]
+point	=	{'x':1,	'y':2}
+point['country'] = 'Vietnam'
+numbers.sort()
+point.clear()
 
-abc = paragraph.strip('3')
-
-print(abc)
-#I want to create a chat socket app by using python
-import socket
-# create a socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# get local machine name
-host = socket.gethostname()
-# define the port on which you want to connect
-port = 12345
-# connect to the server on local computer
-s.connect((host, port))
-# receive data from the server
-msg = s.recv(1024)
-# close the connection
-s.close()
-
+print(point['country'])
