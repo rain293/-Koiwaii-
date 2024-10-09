f20e92a0e2017025d8156d9f767f9fa9789bcbc5
rain293 <twilike9x@gmail.com>
2024-10-09 16:01:24 +0900

    .

--- hello.py
+++ hello.py
@@ -1,15 +1,28 @@
-import json
+from bs4 import BeautifulSoup as Soup
 
-mydata = {
-    'name': 'John',
-    'age': 10
-}
-jsonstring = json.dumps(mydata)
-print(jsonstring)
-print(jsonstring)
-print(jsonstring)
-print(jsonstring)
-print(jsonstring)
-print(jsonstring)
-print(jsonstring)
-print(jsonstring)
+note = '''
+<?xml version="1.0" encoding="UTF-8"?>
+<breakfast_menu>
+    <food>
+        <name>Belgian Waffles</name>
+        <price>$5.95</price>
+        <description>Two of our famous Belgian Waffles with plenty of real maple syrup</description>
+        <calories>650</calories>
+    </food>
+    <food>
+        <name>Strawberry Belgian Waffles</name>
+        <price>$7.95</price>
+        <description>Light Belgian waffles covered with strawberries and whipped cream</description>
+        <calories>900</calories>
+    </food>
+</breakfast_menu>
+'''
+
+soup = Soup(note, 'xml')
+foods = soup.find_all('food')
+for food in foods:
+    name = food.find('name').text
+    price = food.find('price').text
+    description = food.find('description').text
+    calories = food.find('calories').text
+    print(f"{name}: {price}: {description}, calories: {calories}")
