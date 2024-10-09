a3892beb00066386b1f63c359c29aa649a660eac
rain293 <twilike9x@gmail.com>
2024-10-09 18:02:25 +0900

    ád

--- hello.py
+++ hello.py
@@ -1,27 +1,24 @@
 from bs4 import BeautifulSoup
 
-note = '''
-<?xml version="1.0" encoding="UTF-8"?>
-<breakfast_menu>
-    <food>
-        <name>Belgian Waffles</name>
-        <price>$5.95</price>
-        <description>Two of our famous Belgian Waffles with plenty of real maple syrup</description>
-        <calories>650</calories>
-    </food>
-    <food>
-        <name>Strawberry Belgian Waffles</name>
-        <price>$7.95</price>
-        <description>Light Belgian waffles covered with strawberries and whipped cream</description>
-        <calories>900</calories>
-    </food>
-</breakfast_menu>
+html = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Web Page</title>
+</head>
+<body>
+    <h1>Welcome to My Web Page</h1>
+    <p>This is a simple HTML document.</p>
+    <a href="https://example.com">Visit Example.com</a>
+    <div class="content">
+        <h2>About</h2>
+        <p>This section is about something interesting.</p>
+    </div>
+</body>
+</html>
 '''
-soup = BeautifulSoup(note,'xml')
+    
+abc = BeautifulSoup(html,'html.parser')
 
-foods = soup.find_all('food')
-
-for food in foods:
-    name  = food.find('name').text
-    price  = food.find('price').text
-    print(f'{name}:{price}')
+# tcp socket
